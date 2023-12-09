0228568eb71e7e1479730aa4113ac65b185400b1
hasecom <hasetennisth@gmail.com>
2023-12-09 09:47:29 +0900

    input欄ペーストボタン修正

--- src/components/form/inputUrl.tsx
+++ src/components/form/inputUrl.tsx
@@ -1,71 +1,79 @@
-import { useState,useContext } from 'react';
-import { Input,Flex,Button } from '@chakra-ui/react'
+import { useState, useContext } from 'react';
+import { Input, Flex, Button, FormControl, FormErrorMessage } from '@chakra-ui/react'
 import MakerListContext from '@/context/makerListContext';
 import { getAsin } from '@/utill/asin';
 import { fetch } from '@/utill/axios';
 type Props = {
-  number:number
+  number: number
 }
-const InputUrl:React.FC<Props> = ({number}) => {
-  const [inputValue, setInputValue] = useState('');
+const InputUrl: React.FC<Props> = ({ number }) => {
+  const [inputValue, setInputValue] = useState<string>('');
+  const [error, setError] = useState('');
   const makeListContext = useContext(MakerListContext);
-  const handleChange = async(e:any) => {
-    setInputValue(e.target.value);
-    if (inputValue === null) {
-      makeListContext?.removeItemByIndex(number);
-    } else {
-      //asinチェック
-      const asin = getAsin(e.target.value);
-      if(!asin) return;
-      const postData = {'asin':asin}
-      const response = await fetch(postData,'https://hasecom.angry.jp/amazon-qr-maker/request.php');
-      if(response && response.data){ 
-        const responseData = response.data;
-        if(responseData['code'] && responseData['code'] != 0) return;
-        console.log();
-        makeListContext?.changeMakeList(
-          asin, 
-          number,
-          responseData.result.image,
-          responseData.result.price,
-          responseData.result.title,
-          responseData.result.url
-          );
-      }
 
-      
-    }
-  };
-
-  const handlePaste = async () => {
-    if(!navigator.clipboard){
+  const handleChange = (e: any) => {
+    if(e.target.value != null) getProduct(e.target.value);
+  }
+  const handlePaste =  () => {
+    if (!navigator.clipboard) {
       alert('ペースト機能が、お使いの環境に対応していないか、クリップボードにコピーされていません。');
       return;
     }
     navigator.clipboard.readText()
-    .then(text => {
-      setInputValue(text);
-    })
-    .catch(err => {
-      alert('ペースト機能を使うには、ブラウザの許可設定が必要です。')
-    });
+      .then(text => {
+        getProduct(text);
+      })
+      .catch(err => {
+        alert('ペースト機能を使うには、ブラウザの許可設定が必要です。')
+      });
   };
-
+  const getProduct = async(getInputValue: string) => {
+    try {
+      const decodeInputValue = decodeURIComponent(getInputValue)
+      setInputValue(decodeInputValue);
+      if (inputValue == null) {
+        makeListContext?.removeItemByIndex(number);
+        return;
+      }
+      if (!decodeInputValue || decodeInputValue == "") throw '';
+      //asinチェック
+      const asin = getAsin(decodeInputValue);
+      if (!asin) throw 'URLが正しくありません。';
+      const postData = { 'asin': asin }
+      const response = await fetch(postData, 'https://hasecom.angry.jp/amazon-qr-maker/request.php');
+      const responseData = response.data && response.data;
+      if (!responseData || !responseData['code']) throw '商品の取得に失敗しました。'
+      if (responseData['code'] != 0) throw 'この商品は存在しません。';
+      makeListContext?.changeMakeList(
+        asin,
+        number,
+        responseData.result.image,
+        responseData.result.price,
+        responseData.result.title,
+        responseData.result.url
+      );
+    } catch (error: unknown) {
+      setError(typeof error === 'string' ? error : '');
+    }
+  }
   return (
-    <Flex justifyContent="space-between" alignItems="center" p={2}>
-    <Input
-      flex="1"
-      mr={2}
-      type="text"
-      placeholder={`Amazonの商品リンクを入力【 ${number+1} 】`}
-      _placeholder={{ fontSize: '0.75rem' }}
-      value={inputValue}
-      onChange={handleChange}
-    />
-    <Button size="sm" onClick={handlePaste}>
-      ペースト
-    </Button>
-  </Flex>
+    <FormControl isInvalid={!!error}>
+      <Flex justifyContent="space-between" alignItems="center" p={2}>
+        <Input
+          flex="1"
+          mr={2}
+          type="text"
+          placeholder={`Amazonの商品リンクを入力【 ${number + 1} 】`}
+          _placeholder={{ fontSize: '0.75rem' }}
+          value={inputValue}
+          onChange={handleChange}
+        />
+        <Button size="sm" onClick={handlePaste}>
+          ペースト
+        </Button>
+      </Flex>
+      <FormErrorMessage>{error}</FormErrorMessage>
+    </FormControl>
   );
 };
 export default InputUrl;
