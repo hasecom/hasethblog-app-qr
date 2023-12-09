7f3c529be63f35f45c37ee07aae18f5073b69617
hasecom <hasetennisth@gmail.com>
2023-12-10 02:06:19 +0900

    短縮リンク取得機能追加

--- src/constant.tsx
+++ src/constant.tsx
@@ -1,3 +1,2 @@
-const AMZN_ID = 'test'
-//qrcodemaker-22'
+const AMZN_ID = 'qrcodemaker-22'
 export const AMZN_LINK = (asin:string) => `https://www.amazon.co.jp/dp/${asin}/ref=nosim?tag=${AMZN_ID}`;
