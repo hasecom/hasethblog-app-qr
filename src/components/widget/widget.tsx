07cac1fcb5f8c506758143fda239830fad8c1b89
hasecom <hasetennisth@gmail.com>
2023-12-10 23:29:31 +0900

    プライバシーポリシー追加

--- src/components/widget/widget.tsx
+++ src/components/widget/widget.tsx
@@ -4,8 +4,11 @@ const Widget =  () =>  {
   return (
     <>
       <Box mx={['auto', 'auto', 'auto', '10%', '20%', '30%']} width={['90%', '70%']}>
-        <Box position="relative">
+        <Box position="relative" paddingTop="100px">
           <PrivacyPolicyMd />
+       </Box>
+       <Box position="relative" paddingBottom="300px">
+
        </Box>
       </Box>
     </>
