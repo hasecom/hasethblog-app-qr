4f279ea35d5918c8aa3b7ed1ae3745039b9a43c5
hasecom <hasetennisth@gmail.com>
2023-12-11 21:30:19 +0900

    説明欄改善

--- src/assets/md/privacyPolicyMd.tsx
+++ src/assets/md/privacyPolicyMd.tsx
@@ -3,6 +3,8 @@ import '@/assets/style/custom-markdown.css';
 
 const markdownText = `
 #### プライバシーポリシー・免責事項
+　   
+　 
 ##### 当サービスが利用する情報
 　
 当サービスでは、ASINを元にAmazonの商品URLを取得して、QRコードを生成します。  
@@ -14,15 +16,22 @@ const markdownText = `
 トラフィックデータ収集のためCookieを使用しておりますが、  
 個人情報を含まないデータを収集しております。
 ##### 広告の配信
+　  
 当サービスはGoogleAdSenseおよびAmazonアソシエイト・プログラムの参加者です。
+　  
+　  
 ##### 免責事項
+　  
 当サービスを使用して作成されたリンク先で提供される情報、サービスについては、一切の責任を負いません。  
 当サービスで取得する情報は、Product Advertising API (以下PA-API）を使用しており、  
 Amazonの正確な情報を取得することができますが、生成した画像が古い場合、記載情報が古くなっていることもございます。
+　  
+　  
 ##### お問合せ
+　  
 ご要望、ご用件を記入の上、メールにてご連絡ください。  
-運営者【hasethblog@gmail.com】
-
+運営者 hasethblog@gmail.com
+　  
 `;
 
 
