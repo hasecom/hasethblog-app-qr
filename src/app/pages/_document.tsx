1858e1bb17424d4b8e6f1a0e28ef1dff4f4cf99e
hasecom <hasetennisth@gmail.com>
2023-12-12 08:00:37 +0900

    add fav

--- src/app/pages/_document.tsx
+++ src/app/pages/_document.tsx
@@ -1,6 +1,11 @@
-import NextDocument, { Html, Head, Main, NextScript } from 'next/document';
+import Document, { Html, Head, Main, NextScript, DocumentContext } from 'next/document';
 
-export default class Document extends NextDocument {
+class MyDocument extends Document {
+  static async getInitialProps(ctx: DocumentContext) {
+    const initialProps = await Document.getInitialProps(ctx);
+    return { ...initialProps };
+  }
+
   render() {
     return (
       <Html>
@@ -10,7 +15,8 @@ export default class Document extends NextDocument {
           <link rel="icon" type="image/png" sizes="16x16" href="/favicons/favicon-16x16.png" />
           <link rel="manifest" href="/favicons/site.webmanifest" />
           <link rel="mask-icon" href="/favicons/safari-pinned-tab.svg" color="#000000" />
-          <link rel="shortcut icon" href="/favicons/favicon.ico" /> 
+          <link rel="shortcut icon" href="/favicons/favicon.ico" />
+          <link rel="icon" href="/favicons/favicon.ico" type="image/x-icon" sizes="16x16" />
           <meta name="msapplication-TileColor" content="#ffffff" />
           <meta name="msapplication-config" content="/favicons/browserconfig.xml" /> 
           <meta name="theme-color" content="#ffffff" />
@@ -23,3 +29,5 @@ export default class Document extends NextDocument {
     );
   }
 }
+
+export default MyDocument;
